62055db0c70cec57a459ab7dec01284da5d2268a
Sapirteper0402 <sapir.tper@gmail.com>
2023-12-11 23:28:05 +0200

    Add nav pages

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,7 @@
+export function AboutUs() {
+    return (
+        <section className="home">
+        <h1>AboutUs</h1>
+        </section>
+    )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+    return (
+        <section className="home">
+            <h1>Welcome to MisterEmail HomePage</h1>
+        </section>
+    )
+}
